fc137cfdcadf33a290ce2212b43f68880bf30915
John Blaufuss <johncornblaufuss@gmail.com>
2025-10-04 17:23:38 -0400

    a

--- Assets/Scripts/ui_switch.cs
+++ Assets/Scripts/ui_switch.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ui_switch : MonoBehaviour
 {
+    public GameObject Canvas;
+    public int index = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        Canvas.SetActive(false);
+    }   
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("Spacebar was pressed");
+            Canvas.SetActive(true);
+            Canvas.transform.GetChild(index);
+            Debug.Log(index);
+            index++;
         }
     }
 }
